17cbf38446263718b3947482519c98e29b9917ad
faelmonteiro <rafael-augusto15@hotmail.com>
2026-09-22 16:41:55 -0300

    chore: remover funcionalidade AI_AUTO_SHORT_PROMPT

--- internal/config/config.go
+++ internal/config/config.go
@@ -189,7 +189,6 @@ type Config struct {
 	MaxSteps           int
 	MaxContextTurns    int
 	AutoAllowMultiline bool
-	AutoShortPrompt    bool
 	ActiveProviderFile string
 	ModelsConfig       *ModelsConfigFile
 }
@@ -219,7 +218,6 @@ func Load() *Config {
 		MaxSteps:           getEnvIntOrDefault("AI_AUTO_MAX_STEPS", 6),
 		MaxContextTurns:    getEnvIntOrDefault("MAX_CONTEXT_TURNS", 5),
 		AutoAllowMultiline: getEnvBoolOrDefault("AI_AUTO_ALLOW_MULTILINE", false),
-		AutoShortPrompt:    getEnvBoolOrDefault("AI_AUTO_SHORT_PROMPT", false),
 		ModelsConfig:       cfgFile,
 	}
 
@@ -839,7 +837,6 @@ func (c *Config) Reload() {
 	c.OpenAIModel = getEnvOrDefault("OPENAI_MODEL", "gpt-4o-mini")
 	c.G4FModel = getEnvOrDefault("G4F_MODEL", "gpt-4o")
 	c.AutoAllowMultiline = getEnvBoolOrDefault("AI_AUTO_ALLOW_MULTILINE", false)
-	c.AutoShortPrompt = getEnvBoolOrDefault("AI_AUTO_SHORT_PROMPT", false)
 
 	// Atualiza o provedor ativo
 	c.Provider = c.getSavedProviderLocked()
